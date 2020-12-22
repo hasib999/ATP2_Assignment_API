f24fa34e8da2316a3274c27ce46584bdd94a3eb2
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-12-23 01:25:14 +0600

    /update/html

--- Post_Comment/Post_Comment/Front_End/Content/JS/signup.js
+++ Post_Comment/Post_Comment/Front_End/Content/JS/signup.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
 	var signup = function () {
-
+		if ($("#username").val() != "" && $("#password").val() != "") {
 		$.ajax({
 			url: "http://localhost:59954/api/logins/",
 			method: "POST",
@@ -10,7 +10,7 @@
 				Password: $("#password").val()
 			},
 			complete: function (xmlhttp, status) {
-				if ($("#name").val() != "" && $("#password").val() != "") {
+				/*if ($("#username").val() != "" && $("#password").val() != "") {*/
 					if (xmlhttp.status == 200) {
 						$("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">" + xmlhttp.status + ":" + xmlhttp.statusText + "</div>");
 					}
@@ -18,12 +18,16 @@
 					else {
 						$("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">" + xmlhttp.status + ":" + xmlhttp.statusText + "</div>");
 					}
-				}
+				/*}
 				else {
 					$("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">Fill all the Fields</div>");
-				}
+				}*/
 			}
 		});
+		}
+		else {
+			$("#msg").html("<div class=\"alert alert-danger\" role=\"alert\">Fill all the Fields</div>");
+		}
 	}
 	$("#btnsignup").click(function () {
 		signup();
